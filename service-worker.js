38aae44e7fe701ec3d707cf2745815ae94c8b8a7
sjh-00028 <sjh00028@mix.wvu.edu>
2024-04-15 14:45:22 -0400

    Use a service worker Avoid parsing the json every refresh

---
+++ service-worker.js
@@ -0,0 +1,24 @@
+let words;
+
+const wordsUrl = "https://sjh-00028.github.io/words.json";
+const loadPromise = caches.open("words-cache").then(async wordsCache => {
+    await wordsCache.add(wordsUrl);
+
+    let response = await wordsCache.match(wordsUrl);
+    words = await response.json();
+});
+
+self.addEventListener("message", async e => {
+    await loadPromise;
+
+    let word = e.data;
+    let wordEntries = words[word] || words[word.toLowerCase()];
+
+    while (wordEntries != null && wordEntries.length === 1 && typeof(wordEntries[0]) === "string") { // Alias
+        word = wordEntries[0];
+        wordEntries = words[word] || words[word.toLowerCase()];
+    }
+
+    e.ports[0].start();
+    e.ports[0].postMessage(wordEntries);
+});
