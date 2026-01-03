07ba6578ed99b6a7da101306a3bb03e6feac98cc
sjh-00028 <sjh00028@mix.wvu.edu>
2026-01-03 05:28:34 -0500

    Switch from JSON to a binary format The data is formatted in a way it can be binary searched, removing the time & memory spent parsing a large JSON

--- service-worker.js
+++ service-worker.js
@@ -1,24 +1,107 @@
-let words;
+const wordsUrl = "https://sjh-00028.github.io/words.bin";
 
-const wordsUrl = "https://sjh-00028.github.io/words.json";
+let buffer;
+let dataView
+const textEncoder = new TextEncoder()
+const textDecoder = new TextDecoder()
+
+function byteLength(str) {
+    return textEncoder.encode(str).length;
+}
+
+function readString(pos) {
+    let stringLength = 0
+    while (pos + stringLength < buffer.byteLength && dataView.getUint8(pos + stringLength) != 0)
+        stringLength++
+
+    return textDecoder.decode(buffer.slice(pos, pos + stringLength))
+}
+
+function readEntry(pos) {
+    pos += 12
+
+    let word = readString(pos)
+    pos += byteLength(word) + 1
+    let infoCount = dataView.getUint8(pos++)
+    let infos = []
+    for (let i = 0; i < infoCount; i++) {
+        let type = dataView.getUint8(pos++)
+        if (type == 1) {
+            let formOf = readString(pos)
+            pos += byteLength(formOf) + 1
+
+            infos.push(formOf)
+            continue
+        }
+
+        let ipa = readString(pos)
+        pos += byteLength(ipa) + 1
+        let articles = dataView.getUint8(pos++)
+        
+        let definitionCount = dataView.getUint8(pos++)
+        let definitions = []
+        for (let i = 0; i < definitionCount; i++) {
+            let definition = readString(pos)
+
+            definitions.push(definition)
+            pos += byteLength(definition) + 1
+        }
+
+        let formCount = dataView.getUint8(pos++)
+        let forms = []
+        for (let i = 0; i < formCount; i++) {
+            let form = readString(pos)
+
+            forms.push(form)
+            pos += byteLength(form) + 1
+        }
+
+        infos.push({
+            word: word,
+            ipa: ipa,
+            article: [null, "la", "le", "le,la"][articles],
+            definitions: definitions,
+            forms: forms
+        })
+    }
+
+    return infos
+}
+
+function findEntry(word) {
+    let current = dataView.getUint32(0);
+    let found = new Map()
+
+    while (current != 0xFFFFFFFF) {
+        let currentWord = readString(current + 12)
+        if (found.has(currentWord))
+            break
+        if (currentWord.toLowerCase() == word.toLowerCase())
+            return readEntry(current)
+
+        found.set(currentWord)
+        if (currentWord > word)
+            current = dataView.getUint32(current + 4)
+        else
+            current = dataView.getUint32(current + 8)
+    }
+
+    return null
+}
+
 const loadPromise = caches.open("words-cache").then(async wordsCache => {
     await wordsCache.add(wordsUrl);
 
     let response = await wordsCache.match(wordsUrl);
-    words = await response.json();
+    buffer = await response.arrayBuffer();
+    dataView = new DataView(buffer)
 });
 
 self.addEventListener("message", async e => {
     await loadPromise;
 
     let word = e.data;
-    let wordEntries = words[word] || words[word.toLowerCase()];
-
-    while (wordEntries != null && wordEntries.length === 1 && typeof(wordEntries[0]) === "string") { // Alias
-        word = wordEntries[0];
-        wordEntries = words[word] || words[word.toLowerCase()];
-    }
 
     e.ports[0].start();
-    e.ports[0].postMessage({word: word, entries: wordEntries});
+    e.ports[0].postMessage({word: word, entries: findEntry(word)});
 });
